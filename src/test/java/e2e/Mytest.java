764cf4236a70a81c3f267f64065043437f9349e1
Sander Martens <sander.martens@polteq.com>
2018-12-03 19:49:24 +0100

    added extentReport in POM

--- src/test/java/e2e/Mytest.java
+++ src/test/java/e2e/Mytest.java
@@ -1,12 +1,12 @@
+package e2e;
+
 import factories.BrowserFactory;
 import fixtures.UserFixture;
 import flows.LoginFlow;
 import models.User;
-import org.assertj.core.api.Assertions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 import pages.AuthenticationPage;
 import pages.HomePage;
 import pages.Page;
